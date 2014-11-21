03d4e02fd1177689589942cecd7dde4980b825a7
Lukas Matt <lukas@zauberstuhl.de>
2014-11-21 15:44:43 +0100

    Use URL blob instead of a file

--- jsxc.lib.js
+++ jsxc.lib.js
@@ -4967,8 +4967,48 @@ var jsxc;
             if (Worker) {
                // try to create web-worker
 
+              var dsaWebWorkerBlob = URL.createObjectURL(
+                  new Blob(['(', function() {
+                    ;(function (root) {
+                      "use strict";
+
+                      root.OTR = {}
+                      root.DSA = {}
+                      root.crypto = {
+                        randomBytes: function () {
+                          throw new Error("Haven't seeded yet.")
+                        }
+                      }
+                      function sendMsg(type, val) {
+                        postMessage({ type: type, val: val })
+                      }
+                      onmessage = function (e) {
+                        var data = e.data;
+                        //if (data.imports) imports = data.imports
+                        //importScripts.apply(root, imports);
+
+                        // use salsa20 since there's no prng in webworkers
+                        var state = new root.Salsa20(data.seed.slice(0, 32), data.seed.slice(32))
+                        root.crypto.randomBytes = function (n) {
+                          return state.getBytes(n)
+                        }
+                        if (data.debug) sendMsg('debug', 'DSA key creation started')
+                        var dsa
+                        try {
+                          dsa = new root.DSA()
+                        } catch (e) {
+                          if (data.debug) sendMsg('debug', e.toString())
+                          return
+                        }
+                        if (data.debug) sendMsg('debug', 'DSA key creation finished')
+                        sendMsg('data', dsa.packPrivate())
+                      }
+                    }(this))
+                  }.toString(), ')()'], {type: 'application/javascript'})
+              );
+
                try {
-                  worker = new Worker('/javascripts/otr/dsa-webworker.js');
+                  worker = new Worker(dsaWebWorkerBlob);
                } catch (err) {
                   jsxc.warn('Couldn\'t create web-worker.', err);
                }
