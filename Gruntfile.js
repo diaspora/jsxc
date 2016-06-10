9f9d3d2e50a5a0e5721594c3903ebd4de5391854
Lukas Matt <lukas@zauberstuhl.de>
2016-06-10 11:01:11 +0200

    Fix jshint path-no-string-error with v0.9.2
    Signed-off-by: Lukas Matt <lukas@zauberstuhl.de>

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,6 +17,7 @@ module.exports = function(grunt) {
       target: 'dev',
       jshint: {
          options: {
+            reporterOutput: "",
             jshintrc: '.jshintrc'
          },
          gruntfile: {
